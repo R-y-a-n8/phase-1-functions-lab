72ee2fb0c1658b083a4a28c3988c9a9ef673f2c4
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-09-28 16:16:34 +0300

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+}
+  
+function dstanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    //call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    //passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+    //the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+function distanceFromHqInFeet(start, destination) {
+    //return the fare for the customer
+}
